cde71c664d49792641ae105a6a2f64d704e21599
mechvignesh <35688299+mechvignesh@users.noreply.github.com>
2018-02-05 16:11:23 +0530

    Palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e;
+printf("Enter the three digit number:");
+scanf("%d",&a);
+b=a/100;
+c=(a-(b*100))/10;
+d=a-((b*100)+(c*10));
+e=b+c+d;
+if(a=e)
+printf("Palindrome ");
+else
+printf("Not a palindrome");
+return 0;
+}
