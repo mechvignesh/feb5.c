c164489cd14653995a34a912b84c04dd5f5a5b69
mechvignesh <35688299+mechvignesh@users.noreply.github.com>
2018-02-28 15:40:58 +0530

    large28.c

---
+++ large28.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],j,large;
+for(j=0;j<10;i++)
+{
+	scanf("%d",&a[j]);
+}
+a[0]=large;
+for(j=0;j<10;j++)
+{
+	if(a[j]>large)
+	{
+	large=a[j];
+	}
+}
+printf("%d",large);
+return 0;
+}
