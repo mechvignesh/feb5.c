b0d914b81e08bf17e69de99c832628426d174e38
mechvignesh <35688299+mechvignesh@users.noreply.github.com>
2018-02-28 15:48:11 +0530

    Update bwswap28.c

--- bwswap28.c
+++ bwswap28.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-	int a,b;
-	scanf("%d %d",&a,&b);
-a=a^b;
-b=a^b;
-a=a^b;
-printf("%d %d",a,b);
+	int a,n;
+	scanf("%d %d",&a,&n);
+a=a^n;
+n=a^n;
+a=a^n;
+printf("%d %d",a,n);
 return 0;
 }
